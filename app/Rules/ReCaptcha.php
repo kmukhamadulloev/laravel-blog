8e0662d42e6821ce897534397b1da5f68ebd66a6
Komron Mukhamadulloev <kmukhamadulloev@gmail.com>
2023-11-24 15:38:28 +0300

    Added authorization and registration with Google Recaptcha

---
+++ app/Rules/ReCaptcha.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Rules;
+
+use Closure;
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Support\Facades\Http;
+
+class ReCaptcha implements ValidationRule
+{
+    /**
+     * Run the validation rule.
+     *
+     * @param  \Closure(string): \Illuminate\Translation\PotentiallyTranslatedString  $fail
+     */
+    public function validate(string $attribute, mixed $value, Closure $fail): void
+    {
+        $reCaptcha = Http::asForm()->post('https://www.google.com/recaptcha/api/siteverify', [
+            'secret' => config('services.recaptcha.secret'),
+            'response' => $value,
+            'remoteip' => \request()->ip(),
+        ]);
+
+        if (!$reCaptcha->json('success')) {
+            $fail("The {$attribute} is invalid.");
+        }
+    }
+}
